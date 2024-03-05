4f0a54cef97e9fe6f4ef29263159dcf327d82fc3
deeprajc <deeprajchanda100@gmail.com>
2024-03-05 17:42:18 +0530

    mesaging and calling added

--- engine/db.py
+++ engine/db.py
@@ -1,3 +1,4 @@
+import csv
 import sqlite3
 
 conn = sqlite3.connect('Aniraj.db')
@@ -72,9 +73,9 @@ cursor.execute(query)
 
 
 
-query = "INSERT INTO web_command VALUES (null,'whatsapp web', 'https://web.whatsapp.com/')"
-cursor.execute(query)
-conn.commit()
+# query = "INSERT INTO web_command VALUES (null,'whatsapp web', 'https://web.whatsapp.com/')"
+# cursor.execute(query)
+# conn.commit()
 
 
 
@@ -82,3 +83,46 @@ conn.commit()
 # query = "DELETE FROM web_command WHERE ID = "
 # cursor.execute(query)
 # conn.commit()
+
+
+# cursor.execute("DELETE FROM contacts WHERE id >= 0 AND id <= 3")
+# cursor.execute("DELETE FROM contacts WHERE id >= 0")
+
+# conn.commit()
+
+
+# Create a table with the desired columns
+cursor.execute('''CREATE TABLE IF NOT EXISTS contacts (id integer primary key, name VARCHAR(200), mobile_no VARCHAR(255), email VARCHAR(255) NULL)''')
+
+
+
+
+
+
+
+
+# # Specify the column indices you want to import (0-based index)
+# # Example: Importing the 1st and 3rd columns
+
+# desired_columns_indices = [0, 32]
+
+# # Read data from CSV and insert into SQLite table for the desired columns
+
+# with open('contacts.csv', 'r', encoding='utf-8') as csvfile:
+#     csvreader = csv.reader(csvfile)
+#     for row in csvreader:
+#         selected_data = [row[i] for i in desired_columns_indices]
+#         cursor.execute(''' INSERT INTO contacts (id, 'name', 'mobile_no') VALUES (null, ?, ?);''', tuple(selected_data))
+# conn.commit()
+# conn.close()
+
+
+
+
+
+# query = 'rupraj jio'
+# query = query.strip().lower()
+
+# cursor.execute("SELECT mobile_no FROM contacts WHERE LOWER(name) LIKE ? OR LOWER(name) LIKE ?", ('%' + query + '%', query + '%'))
+# results = cursor.fetchall()
+# print(results[0][0])
